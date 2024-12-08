c817091fa1d5ca80364b9a1b3bf45093e469311d
Morgan Arrington <mjarring96@gmail.com>
2024-12-08 11:04:46 -0500

    Solve day01 part 2

--- src/day01.zig
+++ src/day01.zig
@@ -5,6 +5,7 @@ const ArrayList = std.ArrayList;
 const assert = std.debug.assert;
 const parseInt = std.fmt.parseInt;
 const sort = std.mem.sort;
+const AutoHashMap = std.AutoHashMap;
 
 const data = @embedFile("data/day01.txt");
 const test_data = @embedFile("data/day01_test.txt");
@@ -15,27 +16,22 @@ pub fn main() !void {
 
     const allocator = arena.allocator();
 
-    var left_list = ArrayList(i32).init(allocator);
-    defer left_list.deinit();
-    var right_list = ArrayList(i32).init(allocator);
-    defer right_list.deinit();
+    var left = ArrayList(u32).init(allocator);
+    defer left.deinit();
+    var right = ArrayList(u32).init(allocator);
+    defer right.deinit();
 
-    try readListsSorted(&left_list, &right_list, data);
+    try readListsSorted(&left, &right, data);
 
-    var diffs = ArrayList(u32).init(allocator);
-    defer diffs.deinit();
-    try computeDiffs(left_list.items, right_list.items, &diffs);
-
-    var diff_sum: u32 = 0;
-    for (diffs.items) |diff| {
-        diff_sum += diff;
-    }
+    const diff_sum = try computeDiff(left.items, right.items);
+    const similarity = try computeSimilarity(left.items, right.items, allocator);
 
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Diff Sum: {}\n", .{diff_sum});
+    try stdout.print("Similarity: {}\n", .{similarity});
 }
 
-pub fn readListsSorted(left_list: *ArrayList(i32), right_list: *ArrayList(i32), buffer: []const u8) !void {
+fn readListsSorted(left_list: *ArrayList(u32), right_list: *ArrayList(u32), buffer: []const u8) !void {
     var line_iterator = tokenizeAny(u8, buffer, "\n");
     while (line_iterator.next()) |line| {
         var word_iterator = tokenizeAny(u8, line, " ");
@@ -44,7 +40,7 @@ pub fn readListsSorted(left_list: *ArrayList(i32), right_list: *ArrayList(i32), 
             const is_left = index == 0;
             const is_right = index == 1;
             assert(is_left or is_right);
-            const num = try parseInt(i32, word, 10);
+            const num = try parseInt(u32, word, 10);
             if (is_left) {
                 try left_list.append(num);
             } else {
@@ -53,26 +49,55 @@ pub fn readListsSorted(left_list: *ArrayList(i32), right_list: *ArrayList(i32), 
         }
     }
 
-    sort(i32, left_list.items, {}, comptime std.sort.asc(i32));
-    sort(i32, right_list.items, {}, comptime std.sort.asc(i32));
+    sort(u32, left_list.items, {}, comptime std.sort.asc(u32));
+    sort(u32, right_list.items, {}, comptime std.sort.asc(u32));
 }
 
-fn computeDiffs(left: []i32, right: []i32, diffs: *ArrayList(u32)) !void {
+fn computeDiff(left: []u32, right: []u32) !u64 {
+    var sum: u64 = 0;
     for (left, right) |l, r| {
-        try diffs.append(@abs(l - r));
+        if (l > r) {
+            sum += l - r;
+        } else {
+            sum += r - l;
+        }
     }
+    return sum;
+}
+
+fn computeSimilarity(left: []u32, right: []u32, allocator: std.mem.Allocator) !u32 {
+    var map = AutoHashMap(u32, u32).init(allocator);
+    defer map.deinit();
+
+    for (right) |id| {
+        const result = try map.getOrPut(id);
+        if (result.found_existing) {
+            result.value_ptr.* += 1;
+        } else {
+            result.value_ptr.* = 1;
+        }
+    }
+
+    var sum: u32 = 0;
+    for (left) |id| {
+        if (map.get(id)) |value| {
+            sum += id * value;
+        }
+    }
+
+    return sum;
 }
 
 test "day 01" {
-    var left = ArrayList(i32).init(std.testing.allocator);
+    var left = ArrayList(u32).init(std.testing.allocator);
     defer left.deinit();
-    var right = ArrayList(i32).init(std.testing.allocator);
+    var right = ArrayList(u32).init(std.testing.allocator);
     defer right.deinit();
 
     try readListsSorted(&left, &right, test_data);
 
-    const left_ex = [_]i32{ 1, 2, 3, 3, 3, 4 };
-    const right_ex = [_]i32{ 3, 3, 3, 4, 5, 9 };
+    const left_ex = [_]u32{ 1, 2, 3, 3, 3, 4 };
+    const right_ex = [_]u32{ 3, 3, 3, 4, 5, 9 };
     for (left_ex, left.items) |e, a| {
         try std.testing.expectEqual(e, a);
     }
@@ -80,17 +105,9 @@ test "day 01" {
         try std.testing.expectEqual(e, a);
     }
 
-    var diffs = ArrayList(u32).init(std.testing.allocator);
-    defer diffs.deinit();
-    try computeDiffs(left.items, right.items, &diffs);
-    const diffs_ex = [_]u32{ 2, 1, 0, 1, 2, 5 };
-    for (diffs_ex, diffs.items) |ex, act| {
-        try std.testing.expectEqual(ex, act);
-    }
-
-    var diff_sum: u32 = 0;
-    for (diffs.items) |diff| {
-        diff_sum += diff;
-    }
+    const diff_sum = try computeDiff(left.items, right.items);
     try std.testing.expectEqual(11, diff_sum);
+
+    const similarity = computeSimilarity(left.items, right.items, std.testing.allocator);
+    try std.testing.expectEqual(31, similarity);
 }
