e9ae01e015332a5e966d4bfc9b3fb85df969e909
Morgan Arrington <mjarring96@gmail.com>
2024-12-14 15:09:01 -0500

    Solve day02 part 2

--- src/day02.zig
+++ src/day02.zig
@@ -4,7 +4,7 @@ const data = @embedFile("data/day02.txt");
 const test_data = @embedFile("data/day02_test.txt");
 
 const Report = struct {
-    levels: []const u8,
+    levels: []u8,
     fn isSafe(self: *const Report) bool {
         return self.adjacentInRange() and (self.allAscending() or self.allDescending());
     }
@@ -59,6 +59,16 @@ fn readReports(buffer: []const u8, allocator: std.mem.Allocator) ![]Report {
     return try reports.toOwnedSlice();
 }
 
+fn copyReport(report: Report, skip_index: usize, allocator: std.mem.Allocator) !Report {
+    var levels_copy = std.ArrayList(u8).init(allocator);
+    defer levels_copy.deinit();
+    for (report.levels, 0..) |level, index| {
+        if (index == skip_index) continue;
+        try levels_copy.append(level);
+    }
+    return Report{ .levels = try levels_copy.toOwnedSlice() };
+}
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -74,13 +84,26 @@ pub fn main() !void {
     }
 
     var safe_reports: u32 = 0;
+    var safe_with_dampener: u32 = 0;
     for (reports) |report| {
         if (report.isSafe()) {
             safe_reports += 1;
+            safe_with_dampener += 1;
+        } else {
+            for (report.levels, 0..) |level, index| {
+                _ = level;
+                const report_copy = try copyReport(report, index, allocator);
+                defer allocator.free(report_copy.levels);
+                if (report_copy.isSafe()) {
+                    safe_with_dampener += 1;
+                    break;
+                }
+            }
         }
     }
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Safe Reports: {}\n", .{safe_reports});
+    try stdout.print("Safe Reports with dampener: {}\n", .{safe_with_dampener});
 }
 
 test "day 02" {
@@ -110,4 +133,34 @@ test "day 02" {
     try std.testing.expectEqual(true, reports[5].isSafe());
     try std.testing.expectEqual(true, reports[5].allAscending());
     try std.testing.expectEqual(false, reports[5].allDescending());
+}
+
+test "copy report 1" {
+    var levels = [_]u8{ 1, 2, 3, 4, 5 };
+    var expected_levels = [_]u8{ 1, 3, 4, 5 };
+    const report = Report{ .levels = &levels };
+    const copy: Report = try copyReport(report, 1, std.testing.allocator);
+    defer std.testing.allocator.free(copy.levels);
+    const copy_expected = Report{ .levels = &expected_levels };
+    try std.testing.expectEqualSlices(u8, copy_expected.levels, copy.levels);
+}
+
+test "copy report 2" {
+    var levels = [_]u8{ 1, 2, 3, 4, 5 };
+    var expected_levels = [_]u8{ 2, 3, 4, 5 };
+    const report = Report{ .levels = &levels };
+    const copy: Report = try copyReport(report, 0, std.testing.allocator);
+    defer std.testing.allocator.free(copy.levels);
+    const copy_expected = Report{ .levels = &expected_levels };
+    try std.testing.expectEqualSlices(u8, copy_expected.levels, copy.levels);
+}
+
+test "copy report 3" {
+    var levels = [_]u8{ 1, 2, 3, 4, 5 };
+    var expected_levels = [_]u8{ 1, 2, 3, 4 };
+    const report = Report{ .levels = &levels };
+    const copy: Report = try copyReport(report, 4, std.testing.allocator);
+    defer std.testing.allocator.free(copy.levels);
+    const copy_expected = Report{ .levels = &expected_levels };
+    try std.testing.expectEqualSlices(u8, copy_expected.levels, copy.levels);
 }
